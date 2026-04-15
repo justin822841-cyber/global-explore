ec42922219e522231e84ebbd74bb15029c87a3f8
justin822841-cyber <justin822841@gmail.com>
2026-04-15 16:28:59 +1000

    Update itinerary.js

--- api/itinerary.js
+++ api/itinerary.js
@@ -368,7 +368,7 @@ function parseFlightPrice(text) {
   };
 }
 
-function parsePlainText(text, affiliates, localTransport, anchors, currency) {
+function parsePlainText(text, affiliates, localTransport, anchors, currency, lang) {
   const sections = splitSections(text);
 
   return {
