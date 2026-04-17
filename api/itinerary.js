3de8890ba519339fec0d45c3ced0aa8e7ab2aec4
justin822841-cyber <justin822841@gmail.com>
2026-04-17 11:52:47 +1000

    Update itinerary.js

--- api/itinerary.js
+++ api/itinerary.js
@@ -337,7 +337,7 @@ TRAVEL DAY RULES:
 
     const sanitizedText = fullText
       .replace(/\u2018|\u2019/g, "'")
-      .replace(/\u201C|\u201D/g, '"'')
+      .replace(/\u201C|\u201D/g, '"')
       .replace(/\u2026/g, '...')
       .replace(/\u2013|\u2014/g, '-');
 
